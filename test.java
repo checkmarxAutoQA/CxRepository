73a3c8b1b24fd4bcd2a8268a6f1d313199d490bd
checkmarxAutoQA <65957156+checkmarxAutoQA@users.noreply.github.com>
2020-11-05 18:03:19 +0200

    sdfsadfsadfa
    sadfsdfsdfasf2423423

--- test.java
+++ test.java
@@ -11,7 +11,7 @@ public class SQLInjection
 		 high(new TextBox());
 	     //test1111199999
 	     //test2222333334444666666777788889999444000sdfsdfsd9asdasd99sdfsdf888hghjghj89sdfsdf99cvcxcvxcv9xcdsfsdfsdvxcvsdfsdddd4deee5tttt90909ttttttt
-	     //111666ddddddddddddddddddcvcvbnvbadsfdsafsdfsdf
+	     //111666ddddddddddddddddddcvcvbnvbadsfdsafsdfsdfsdadsfasdf
 	     ///LizaHF14dddddddddddddsdfsdfdcvcvvbnsdfsdfsdfvbnvb
 	     //ddddmmmmmmsss
 	     //ddddmmmmmmdsfasdf
